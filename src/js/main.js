2d5fb9555b037215005900b1d1735b8b191eee5a
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-08-08 17:35:54 +0300

    remove comment

--- src/js/main.js
+++ src/js/main.js
@@ -253,36 +253,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 
-	// const course = 3.2
-	// getResource('http://localhost:3000/menu').then(data => createCard(data))
-
-	// function createCard(data) {
-	// 	data.forEach(({ img, altimg, title, descr, price }) => {
-	// 		const element = document.createElement('div')
-	// 		element.classList.add('menu__item')
-	// 		element.innerHTML = `
-	// 				<img src=${img} alt=${altimg} />
-	// 				<h3 class="menu__item-subtitle">${title}</h3>
-	// 				<div class="menu__item-descr">
-	// 					${descr}
-	// 				</div>
-	// 				<div class="menu__item-divider"></div>
-	// 				<div class="menu__item-price">
-	// 					<div class="menu__item-cost">Цена:</div>
-	// 					<div class="menu__item-total">
-	// 						<span>${changeToBel(price, course)}</span> BYN/день
-	// 					</div>
-	// 				</div>
-	// 			`
-
-	// 		document.querySelector('.menu .container').append(element)
-	// 	})
-	// }
-
-	// function changeToBel(price, course) {
-	// 	return +parseFloat(price * course).toFixed(2)
-	// }
-
 	// Send form
 
 	const forms = document.querySelectorAll('form')
