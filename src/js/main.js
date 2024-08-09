6c0556bc0eb15fec9efa362438900b2e65672c98
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-08-09 14:20:03 +0300

    add base calc

--- src/js/main.js
+++ src/js/main.js
@@ -428,4 +428,82 @@ document.addEventListener('DOMContentLoaded', () => {
 			slideParameters()
 		})
 	})
+
+	// Calc
+	const result = document.querySelector('.calculating__result span')
+	let gender = 'female',
+		height,
+		weight,
+		age,
+		ratio = 1.375
+
+	function calcTotal() {
+		if (!gender || !height || !weight || !age || !ratio) {
+			result.textContent = '____'
+			return
+		}
+
+		if (gender === 'female') {
+			result.textContent = Math.round(
+				(447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+			)
+		} else {
+			result.textContent = Math.round(
+				(88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+			)
+		}
+	}
+
+	calcTotal()
+
+	function getStaticInformation(parentSelector, activeClass) {
+		const elements = document.querySelectorAll(`${parentSelector} div`)
+
+		elements.forEach(element => {
+			element.addEventListener('click', event => {
+				if (event.target.getAttribute('data-radio')) {
+					ratio = +event.target.getAttribute('data-radio')
+				} else {
+					gender = event.target.getAttribute('id')
+				}
+
+				elements.forEach(item => {
+					item.classList.remove(activeClass)
+				})
+
+				event.target.classList.add(activeClass)
+				calcTotal()
+			})
+		})
+	}
+
+	getStaticInformation('#gender', 'calculating__choose-item_active')
+	getStaticInformation(
+		'.calculating__choose_big',
+		'calculating__choose-item_active'
+	)
+
+	function getDynamicInformation(selector) {
+		const input = document.querySelector(selector)
+
+		input.addEventListener('input', function () {
+			switch (input.getAttribute('id')) {
+				case 'height':
+					height = +input.value
+					break
+				case 'weight':
+					weight = +input.value
+					break
+				case 'age':
+					age = +input.value
+					break
+			}
+
+			calcTotal()
+		})
+	}
+
+	getDynamicInformation('#height')
+	getDynamicInformation('#weight')
+	getDynamicInformation('#age')
 })
